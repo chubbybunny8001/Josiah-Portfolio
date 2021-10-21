78bebc5af3f7ea992881d6cfbecee5f1fd3f1894
Josiah Sanchez <josiah.rasanchez@gmail.com>
2021-10-20 20:04:29 -0600

    minor changes

--- src/components/Experience/ExperienceContent.js
+++ src/components/Experience/ExperienceContent.js
@@ -1,7 +1,7 @@
 export const cont1 = {
             head: 'OpenMic Home Page',
             body: 'This was the wireframe I made to mimic the way the application would work on a desktop/laptop.',
-            picture: require('../../images/CompView.png').default,
+            picture: require('../../images/openMic.png').default,
 }
 export const cont2 = {
             head: 'Wireframe 1',
@@ -11,7 +11,7 @@ export const cont2 = {
 export const cont3 = {
             head: 'Wireframe 2',
             body: 'This was the wireframe I made to mimic the way the application would work on a desktop/laptop.',
-            picture: require('../../images/CompView.png').default,
+            picture: require('../../images/Mobile.png').default,
 }
 export const hiddenCont ={
             head: 'Hidden Container',
